19ee56271a533f5ed4e0f2e809ff174a0b505dde
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2014-08-30 13:55:48 +0200

    Added CharToInt() function

--- NumberToStringConvert/main.cpp
+++ NumberToStringConvert/main.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+int CharToInt(char c);
+
 int main(int argc, char* argv[])
 {
     cout << "Examples of how convert number to string and string to number." << endl;
+
+    cout << CharToInt('7') << endl;
     return 0;
 }
+
+#pragma region Functions
+int CharToInt(char c)
+{
+    return c - '0';
+}
+#pragma endregion
