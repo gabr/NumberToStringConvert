92c334ce43567941f9453fefcc7c6b896955e2ee
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2014-08-30 16:56:06 +0200

    Add conversion from one digit int to char
    Function that converts one digit number to char.
    If number is negative or has more then one digit, function will return
    '0'.

--- NumberToStringConvert/main.cpp
+++ NumberToStringConvert/main.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 int CharToInt(char c);
+char OneDigitIntToChar(int i);
 
 int main(int argc, char* argv[])
 {
     cout << "Examples of how convert number to string and string to number." << endl;
 
     cout << CharToInt('7') << endl;
+    cout << OneDigitIntToChar(13) << " " << OneDigitIntToChar(-7) << " " << OneDigitIntToChar(3) << endl;
     return 0;
 }
 
@@ -16,5 +18,13 @@ int main(int argc, char* argv[])
 int CharToInt(char c)
 {
     return c - '0';
+}
+
+char OneDigitIntToChar(int i)
+{
+    if (i > 9 || i < 0)
+        return '0';
+    
+    return i + '0';
 }
 #pragma endregion
