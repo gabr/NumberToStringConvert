0bfaee7ec23b079b3ce7713bb428c6aeb8a807c2
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2014-08-30 13:40:54 +0200

    init

---
+++ NumberToStringConvert/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "Examples of how convert number to string and string to number." << endl;
+    return 0;
+}
